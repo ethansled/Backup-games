a6d7a96c800fa9c4a6aa074f7898e656100a5695
Ethan Sledzinski <ethansled@gmail.com>
2022-10-12 12:24:20 -0400

    adding a base

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const ejs = require("ejs");
+
+const app = new express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.set("view engine","ejs");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/" , (req,res) => {
+    res.render("index");
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is Running on ${PORT}`));
