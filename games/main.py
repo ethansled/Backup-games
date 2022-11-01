b39f2e138d7b7ef39dcba014091e01d1ee7494ac
Andrew Sheldrick <ajsheld12@gmail.com>
2022-11-01 00:38:30 -0400

    incremental

---
+++ games/main.py
@@ -0,0 +1,22 @@
+import asyncio
+import pygame
+
+pygame.init()
+pygame.display.set_mode((320, 240))
+pygame.display.set_caption("TEST")
+
+
+async def main():
+    count = 3
+
+    while True:
+        print(f"""Hello[{count}] from Pygame""")
+        pygame.display.update()
+        await asyncio.sleep(0)  # very important, and keep it 0
+
+        if not count:
+            pygame.quit()
+            return
+        count = count - 1
+
+asyncio.run( main() )
