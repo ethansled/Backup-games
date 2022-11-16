2d5702f0e8a83c5ce1e9a6f79ef54e8bf8aa0022
tthompson0820 <thomasthompson0820@gmail.com>
2022-11-16 10:37:43 -0500

    Added some updates

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 const express = require("express");
 const ejs = require("ejs");
 
-const ContainerManager = require("./containers.js")
-
 const app = new express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/" , (req,res) => {
+app.get("/", (req, res) => {
     res.render("index");
 });
 
 app.get("/games/:appId", (req, res) => {
-    
+
 })
 
-app.get("/tower-defense" , function(req,res) {
+app.get("/tower-defense", function(req, res) {
     res.render("../views/tower-defense.ejs");
 });
 
-app.get("/retro-racing" , function(req,res) {
+app.get("/retro-racing", function(req, res) {
     res.render("../views/retro-racing.ejs");
 });
 
-app.get("/black-jack" , function(req,res) {
+app.get("/black-jack", function(req, res) {
     res.render("../views/black-jack.ejs");
 });
 
