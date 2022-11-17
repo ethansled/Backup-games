b31ab0b1d6408f5931fc21be97c39b7bc8fe3346
Ethan Sledzinski <ethansled@gmail.com>
2022-11-17 16:29:30 -0500

    fixed server start changed file path

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("../public/views/index.ejs");
 });
 
 app.get("/games/:appId", (req, res) => {
